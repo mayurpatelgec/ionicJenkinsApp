ff9d70302b696332bb199a39c6e69c7ff0c317e1
Dhara Patel <dharabhimani94@gmail.com>
2018-10-03 12:24:54 +0200

    added sonar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,14 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    var a = NaN;
 
+if (a === NaN) {  // Noncompliant; always false
+  console.log("a is not a number");  // this is dead code
+}
+if (a !== NaN) { // Noncompliant; always true
+  console.log("a is not NaN"); // this statement is not necessarily true
+}
   }
 
 }
